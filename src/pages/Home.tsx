1a34e4cf48bd912e7f1152e6e6cb0c4a7e6db6ed
pedroHen14 <pedrohenrique_silvasantos@yahoo.com>
2021-06-24 23:21:58 -0300

    feat: adicionada validação para caso o usuário tente entrar em uma sala que não esteja registrada no banco de dados

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,7 @@ import { Button } from '../components/Button';
 import '../styles/auth.scss';
 import { useAuth } from '../hooks/useAuth';
 import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
 
 export function Home() {
 	const history = useHistory();
@@ -24,6 +25,19 @@ export function Home() {
 
 	async function handleJoinRoom(event: FormEvent) {
 		event.preventDefault();
+
+		if(roomCode.trim() === '') {
+			return;
+		}
+
+		const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+		if(!roomRef.exists()) {
+			alert('Room this not exists');
+			return;
+		}
+
+		history.push(`/rooms/${roomCode}`);
 	}
 
 	return(
@@ -41,7 +55,7 @@ export function Home() {
 						Crie sua sala com o Google
 					</button>
 					<div className="separator">ou entre em uma sala</div>
-					<form action="">
+					<form onSubmit={handleJoinRoom}>
 						<input 
 							type="text"
 							placeholder="Digite o código da sua sala" 
